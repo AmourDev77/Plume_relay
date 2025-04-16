001054ff05614d1cbb0ec6cd37e40e3e9981c796
TMCooper <pokemon160377@gmail.com>
2025-04-17 01:00:16 +0200

    Update Migration

--- migrations/2025-04-16-203220_plume/down.sql
+++ migrations/2025-04-16-203220_plume/down.sql
@@ -1,2 +1,4 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE User;
+DROP TABLE Messages;
+DROP TABLE Autorization;
--- src/schema.rs
+++ src/schema.rs
@@ -1,9 +1,45 @@
 // @generated automatically by Diesel CLI.
 
 diesel::table! {
+    Autorization (id) {
+        id -> Integer,
+        user_id -> Integer,
+        #[max_length = 200]
+        token -> Varchar,
+        #[sql_name = "type"]
+        #[max_length = 50]
+        type_ -> Varchar,
+    }
+}
+
+diesel::table! {
+    Messages (id) {
+        id -> Integer,
+        #[max_length = 100]
+        author -> Varchar,
+        createdAt -> Timestamp,
+        #[max_length = 10000]
+        message_content -> Varchar,
+    }
+}
+
+diesel::table! {
     User (id) {
         id -> Integer,
         #[max_length = 100]
         username -> Varchar,
+        #[max_length = 200]
+        password -> Varchar,
+        #[max_length = 200]
+        email -> Varchar,
+        createdAt -> Timestamp,
+        #[max_length = 400]
+        banner -> Varchar,
     }
 }
+
+diesel::allow_tables_to_appear_in_same_query!(
+    Autorization,
+    Messages,
+    User,
+);
