1a3a20d4bc83bd3ff2eb5552091d8c40eed938b3
Tokiwadai-Railgun <arthur.scheidbach@proton.me>
2025-04-20 10:45:56 +0200

    Finished rust websocket server, broadcasting every messages it receive

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-use diesel::prelude::*;
-use std::env;
-
-pub fn establish_connection() -> MysqlConnection {
-    let database_url = env::var("DATABASE_URL").expect("Missing DATABASE_URL environment variable");
-
-
-}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,74 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use std::{
+    collections::HashMap,
+    env,
+    io::Error as IoError,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Welcome to this api, please feel free to visit Github Wiki to get a reference")
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::protocol::Message;
+
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+
+async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: SocketAddr) {
+    println!("Incoming TCP connection from: {}", addr);
+
+    let ws_stream = tokio_tungstenite::accept_async(raw_stream)
+        .await
+        .expect("Error during the websocket handshake occurred");
+    println!("WebSocket connection established: {}", addr);
+
+    // Insert the write part of this peer to the peer map.
+    let (tx, rx) = unbounded();
+    peer_map.lock().unwrap().insert(addr, tx);
+
+    let (outgoing, incoming) = ws_stream.split();
+
+    let broadcast_incoming = incoming.try_for_each(|msg| {
+        println!("Received a message from {}: {}", addr, msg.to_text().unwrap());
+        let peers = peer_map.lock().unwrap();
+
+        // We want to broadcast the message to everyone except ourselves.
+        let broadcast_recipients =
+            peers.iter().filter(|(peer_addr, _)| peer_addr != &&addr).map(|(_, ws_sink)| ws_sink);
+
+        for recp in broadcast_recipients {
+            let message = Message::text(format!("[{}] - {}",addr, msg.clone()));
+            recp.unbounded_send(message).unwrap();
+        }
+
+        future::ok(())
+    });
+
+    let receive_from_others = rx.map(Ok).forward(outgoing);
+
+    pin_mut!(broadcast_incoming, receive_from_others);
+    future::select(broadcast_incoming, receive_from_others).await;
+
+    println!("{} disconnected", &addr);
+    peer_map.lock().unwrap().remove(&addr);
 }
 
-const BIND_ADDRESS: &str = "127.0.0.1";
-const BIND_PORT: u16 = 8080;
+#[tokio::main]
+async fn main() -> Result<(), IoError> {
+    let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8081".to_string());
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    println!("Starting server on {BIND_ADDRESS}:{BIND_PORT}");
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-    })
-    .bind((BIND_ADDRESS, BIND_PORT))? // Bind address and port of the api, once in production the url will be replaced by 0.0.0.0 to enable requests from all origins
-    .run()
-    .await
+    let state = PeerMap::new(Mutex::new(HashMap::new()));
+
+    // Create the event loop and TCP listener we'll accept connections on.
+    let try_socket = TcpListener::bind(&addr).await;
+    let listener = try_socket.expect("Failed to bind");
+    println!("Listening on: {}", addr);
+
+    // Let's spawn the handling of each connection in a separate task.
+    while let Ok((stream, addr)) = listener.accept().await {
+        tokio::spawn(handle_connection(state.clone(), stream, addr));
+    }
+
+    Ok(())
 }
